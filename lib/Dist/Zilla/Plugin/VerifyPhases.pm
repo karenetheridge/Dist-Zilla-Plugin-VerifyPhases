c765a584e146ecd5e1451b08f004b3da4b2adc2a
Karen Etheridge <ether@cpan.org>
2014-02-22 21:08:24 -0800

    initial code, docs, tests

--- lib/Dist/Zilla/Plugin/VerifyPhases.pm
+++ lib/Dist/Zilla/Plugin/VerifyPhases.pm
@@ -1,10 +1,69 @@
 use strict;
 use warnings;
 package Dist::Zilla::Plugin::VerifyPhases;
-# ABSTRACT: ...
+# ABSTRACT: Compare data and files at different phases of the distribution build process
 # vim: set ts=8 sw=4 tw=78 et :
 
+use Moose;
+with
+    'Dist::Zilla::Role::FileGatherer',
+    'Dist::Zilla::Role::FileMunger',
+    'Dist::Zilla::Role::AfterBuild';
+use Moose::Util 'find_meta';
+use Digest::MD5 'md5_hex';
+use namespace::autoclean;
 
+my %all_files;
+
+sub gather_files
+{
+    my $self = shift;
+
+    my $distmeta_attr = find_meta($self->zilla)->find_attribute_by_name('distmeta');
+    $self->log('distmeta has already been calculated after file gathering phase!')
+        if $distmeta_attr->has_value($self->zilla);
+}
+
+sub munge_files
+{
+    my $self = shift;
+
+    foreach my $file (@{$self->zilla->files})
+    {
+        # don't force FromCode files to calculate early; it might fire some
+        # lazy attributes prematurely
+        $all_files{$file->name} = $file->isa('Dist::Zilla::File::FromCode')
+            ? 'content ignored'
+            : md5_hex($file->encoded_content);
+    }
+}
+
+sub after_build
+{
+    my $self = shift;
+
+    foreach my $file (@{$self->zilla->files})
+    {
+        if (not exists $all_files{$file->name})
+        {
+            $self->log('file has been added after munging phase: \'' . $file->name . '\'');
+            next;
+        }
+
+        # we give FromCode files a bye, since there is a good reason why their
+        # content at file munging time is incomplete
+        $self->log('content has changed after munging phase: \'' . $file->name . '\'')
+            if not $file->isa('Dist::Zilla::File::FromCode')
+                and $all_files{$file->name} ne md5_hex($file->encoded_content);
+
+        delete $all_files{$file->name};
+    }
+
+    foreach my $file (keys %all_files)
+    {
+        $self->log('File has been removed after munging phase: \'' . $file . '\'');
+    }
+}
 
 1;
 __END__
@@ -13,37 +72,44 @@ __END__
 
 =head1 SYNOPSIS
 
-    use Dist::Zilla::Plugin::VerifyPhases;
+In your F<dist.ini>, as the last plugin loaded:
 
-    ...
+    [VerifyPhases]
 
 =head1 DESCRIPTION
 
-...
+This plugin runs in multiple L<Dist::Zilla> phases to check what actions have
+taken place so far.  Its intent is to find any plugins that are performing
+actions outside the appropriate phase, so they can be fixed.
 
-=head1 FUNCTIONS/METHODS
+Running at the end of the C<-FileGatherer> phase, it verifies that the
+distribution's metadata has not yet been calculated (as it usually depends on
+knowing the full manifest of files in the distribution).
 
-=head2 C<foo>
+It runs at the C<-FileMunger> and C<-AfterBuild> phases to record the state
+of files after they have been munged, and again at the end of the build
+process.  Any files that have had their names or content changed are flagged.
 
-...
+Currently, L<FromCode|Dist::Zilla::File::FromCode> files are not checked for
+content, as interesting side effects can occur if their content subs are run
+before all content is available (for example, other lazy builders can run too
+early, resulting in incomplete or missing data).
 
 =head1 SUPPORT
 
 =for stopwords irc
 
-Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Dist-Zilla-Plugin-VerifyPhases>
-(or L<bug-Dist-Zilla-Plugin-VerifyPhases@rt.cpan.org|mailto:bug-Dist-Zilla-Plugin-VerifyPhases@rt.cpan.org>).
+Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Dist-Zilla-PluginBundle-Author-ETHER>
+(or L<bug-Dist-Zilla-PluginBundle-Author-ETHER@rt.cpan.org|mailto:bug-Dist-Zilla-PluginBundle-Author-ETHER@rt.cpan.org>).
 I am also usually active on irc, as 'ether' at C<irc.perl.org>.
-
-=head1 ACKNOWLEDGEMENTS
-
-...
 
 =head1 SEE ALSO
 
 =begin :list
 
-* L<foo>
+* L<Dist::Zilla::Plugin::ReportPhase>
+
+* L<Dist::Zilla::App::Command::dumpphases>
 
 =end :list
 
--- t/01-basic.t
+++ t/01-basic.t
@@ -3,8 +3,59 @@ use warnings FATAL => 'all';
 
 use Test::More;
 use if $ENV{AUTHOR_TESTING}, 'Test::Warnings';
-use Dist::Zilla::Plugin::VerifyPhases;
+use Test::Fatal;
+use Test::Deep;
+use Test::DZil;
 
-fail('this test is TODO!');
+{
+    package Dist::Zilla::Plugin::Naughty;
+    use Moose;
+    with
+        'Dist::Zilla::Role::FileGatherer',
+        'Dist::Zilla::Role::PrereqSource',
+        'Dist::Zilla::Role::FileInjector';
+
+    sub gather_files
+    {
+        my $self = shift;
+        my $distmeta = $self->zilla->distmeta;  # make the attribute fire
+    }
+    sub register_prereqs
+    {
+        my $self = shift;
+        require Dist::Zilla::File::InMemory;
+        $self->add_file( Dist::Zilla::File::InMemory->new(
+            name => 'rogue_file',
+            content => 'naughty naughty!',
+        ));
+    }
+}
+
+{
+    my $tzil = Builder->from_config(
+        { dist_root => 't/does_not_exist' },
+        {
+            add_files => {
+                'source/dist.ini' => simple_ini(
+                    [ Naughty => ],
+                    [ VerifyPhases => ],
+                ),
+            },
+        },
+    );
+
+    $tzil->chrome->logger->set_debug(1);
+    $tzil->build;
+
+    cmp_deeply(
+        $tzil->log_messages,
+        supersetof(
+            '[VerifyPhases] distmeta has already been calculated after file gathering phase!',
+            '[VerifyPhases] file has been added after munging phase: \'rogue_file\'',
+        ),
+        'warnings are logged about our naughty plugin',
+    )
+    or diag explain $tzil->log_messages;
+}
 
 done_testing;
