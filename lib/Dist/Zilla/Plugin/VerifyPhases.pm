a6504d7a87808e7e59da53867cc28074542af295
Karen Etheridge <ether@cpan.org>
2015-08-21 23:07:25 -0700

    also track the _share_dir_map attribute

--- lib/Dist/Zilla/Plugin/VerifyPhases.pm
+++ lib/Dist/Zilla/Plugin/VerifyPhases.pm
@@ -94,7 +94,7 @@ sub gather_files
     my $zilla = $self->zilla;
     my $meta = find_meta($zilla);
 
-    foreach my $attr_name (qw(name version release_status abstract main_module authors distmeta))
+    foreach my $attr_name (qw(name version release_status abstract main_module authors distmeta _share_dir_map))
     {
         next if exists $zilla_constructor_args{$attr_name};
         my $attr = $meta->find_attribute_by_name($attr_name);
@@ -359,6 +359,7 @@ distribution, with known encodings:
 * license
 * authors
 * metadata
+* _share_dir_map
 
 Running at the end of the C<-EncodingProvider> phase, it forces all encodings
 to be built (by calling their lazy builders), to use their C<SetOnce> property
